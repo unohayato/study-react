68723eefaff4af2453f6324f6d555d749655171d
unohayato <mamidori1p1p@gmail.com>
2023-03-05 23:53:38 +0900

    feat: :fire: delete modules

--- components/Headline.js
+++ components/Headline.js
@@ -1,12 +1,3 @@
-import Head from 'next/head'
-import { Footer } from '@/components/Footer'
-import { Main } from '@/components/Main'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export function Headline(props) {
   return (
     <div>
